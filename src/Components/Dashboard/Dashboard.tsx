307242ae369f54c664add4116a885760cade7906
Ryan Baer <ryanjbaer@outlook.com>
2023-05-18 11:00:53 -0600

    add intro paragraph

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -3,7 +3,8 @@ import './Dashboard.scss'
 const Dashboard = () => {
     return (
         <div>
-
+            <p>Front-End Typescript/React developer continuously looking for new knowledge and currently residing in Denver, Colorado. Following a childhood love for video games and technology; I dove into the world of software development. 
+               I fiddled around on various online courses, and then honed my skills at Turing School of Software and Design.</p>
         </div>
     )
 }
