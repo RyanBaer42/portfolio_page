5c0f644f31023e2f842814f1f4c6c340107d4b0e
Ryan Baer <ryanjbaer@outlook.com>
2023-05-18 11:02:04 -0600

    add styling for nav

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -3,7 +3,13 @@ import './Nav.scss'
 const Nav = () => {
     return (
         <div className='Nav'>
-            
+            <p className='home-button'>Ryan Baer</p>
+            <div className='options'>
+                <p className='nav-button'>Projects</p>
+                <p className='nav-button'>Skills</p>
+                <p className='nav-button'>About Me</p>
+                <button className='contact-button'>Contact</button>
+            </div>
         </div>
     )
 }
