9bcf219685609a1f0976c0a40535fd86cada206e
Ryan Baer <ryanjbaer@outlook.com>
2023-06-13 10:49:56 -0600

    add project to project array

--- src/projects.ts
+++ src/projects.ts
@@ -10,8 +10,15 @@ const projects= [
         name: 'Backcountry Bookings',
         image: '/Gifs/backcountry.gif',
         skills: ['React', 'React Router', 'SCSS', 'TypeScript', 'AWS s3'],
-        overView: 'Your ultimate destination for all your camping needs! This website is a comprehensive conglomerate of campsite information from around the country, designed to provide campers with an easy-to-navigate and reliable platform for finding the perfect campsite for their outdoor adventures.',
+        overView: 'This website is a comprehensive conglomerate of campsite information from around the country, designed to provide campers with an easy-to-navigate and reliable platform for finding the perfect campsite for their outdoor adventures.',
         link: 'https://github.com/Backcountry-Bookings/backcountry_fe'
+    },
+    {
+        name: 'Fit Lit',
+        image: '/Gifs/fitlit.gif',
+        skills: ['JavaScript', 'Chart.js', 'CSS', 'HTML5', 'RESTful API'],
+        overView: 'Tracking progress while going through your health and wellness journey can be difficult and complicated. Fortunately FitLit is here to save the day! Through this web app user data can be collected, stored, and displayed to the user in truly meaningful ways.',
+        link: 'https://github.com/smithkirsten/fitlit-group-project'
     }
 ]
 
