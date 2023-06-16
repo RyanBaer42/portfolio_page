b1340c1e6fb434ff7967a2ecbfe23c47b5fbc2e9
Ryan Baer <ryanjbaer@outlook.com>
2023-06-15 23:33:32 -0600

    display skill logos

--- src/Components/SkillsPage/SkillsPage.tsx
+++ src/Components/SkillsPage/SkillsPage.tsx
@@ -1,17 +1,29 @@
 import './SkillsPage.scss'
+import JSLogo from "../../Media/JavaScript.png"
+import TSLogo from "../../Media/Typescript.png"
+import HTMLLogo from "../../Media/HTML.png"
+import CSSLogo from "../../Media/CSS.png"
+import PostreSQLLogo from "../../Media/Postgresql.png"
+import CypressLogo from "../../Media/Cypress.png"
+import SASSLogo from "../../Media/Sass.png"
+import ReactLogo from "../../Media/React.png"
+import MochaLogo from "../../Media/Mocha.png"
+// import ChaiLogo from "../../Media/Chai.png"
 
 const SkillsPage = () => {
     return (
         <div className='skills-page'>
             <div className='skills-container'>
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
+                <img src={JSLogo} alt='Javascript Logo' className='skill-logo'/>
+                <img src={TSLogo} alt='TypeScript Logo' className='skill-logo'/> 
+                <img src={ReactLogo} alt='React Logo' className='skill-logo'/>
+                <img src={HTMLLogo} alt='HTML Logo' className='skill-logo'/>
+                <img src={CSSLogo} alt='CSS Logo' className='skill-logo'/>
+                <img src={PostreSQLLogo} alt='Postres Logo' className='skill-logo'/>
+                <img src={CypressLogo} alt='Cypress Logo' className='skill-logo'/>
+                <img src={SASSLogo} alt='Sass logo' className='skill-logo'/>
+                <img src={MochaLogo} alt='Mocha Logo' className='skill-logo'/>
+                {/* <img src={ChaiLogo} alt='Chai Logo' className='skill-logo'/> */}
             </div>
         </div>
     )
