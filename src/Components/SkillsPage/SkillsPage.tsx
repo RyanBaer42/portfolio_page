752a7d5a23cc48e3f2978966c97636fe73ecc1e0
Ryan Baer <ryanjbaer@outlook.com>
2023-06-15 23:10:15 -0600

    add language logos

--- src/Components/SkillsPage/SkillsPage.tsx
+++ src/Components/SkillsPage/SkillsPage.tsx
@@ -4,7 +4,14 @@ const SkillsPage = () => {
     return (
         <div className='skills-page'>
             <div className='skills-container'>
-                
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
             </div>
         </div>
     )
